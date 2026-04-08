890f00a7b89444ca8bb86386014e7596d14d2c46
Loevl <83883584+Loevl@users.noreply.github.com>
2026-04-08 18:00:18 +0100

    add assertions to main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <kv.h>
+#include <assert.h>
 
 int main() {
     kv_t *table = kv_init(3);
     printf("%p\n", table);
     printf("%ld\n", table->capacity);
+
+    // Testing
+    kv_t *db = kv_init(16);
+    assert(db != NULL);
+    assert(db->capacity == 16);
+    assert(db->count == 0);
 }
